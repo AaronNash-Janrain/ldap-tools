e3f5e533086a66e34236420689444a96d01d638b
Aaron Nash <anash@janrain.com>
2015-12-30 16:41:21 -0800

    Cleans up arg parser, removes global args

--- ldaptools.py
+++ ldaptools.py
@@ -15,7 +15,7 @@ def num_entries(s):
         else:
             raise Exception
     except Exception:
-        raise argparse.ArgumentTypeError("Accepts 'ENTRY-TYPE' | 'ENTRY-TYPE:N(>0)'")
+        raise argparse.ArgumentTypeError("Accepts input in the form of user|group|sudoer[:N>0]")
 
 def make_interactive(entry_list, bill):
     entry_list = []
@@ -40,7 +40,7 @@ def init_add(args):
     # Do general setup tasks
     #init(args)
     new_entries = []
-    collection = {}
+    collection = None
     # Read in existing entries from input file if -r
     # ...
     # Check the types/number of entries read and walk through prompt for each entry if -i
@@ -52,8 +52,9 @@ def init_add(args):
         if args.ldif_out_path == None
         else open(args.ldif_out_path, 'w')
     )
-    for entry in collection['entries']:
-        out.write(entry['dn'], ldifutils.get_add_mod_list(entry['entry']))
+    if collection != None:
+        for entry in collection['entries']:
+            out.write(entry['dn'], ldifutils.get_add_mod_list(entry['entry']))
     if args.ldif_out_path != None: print "LDIF written to %s" % args.ldif_out_path
     # Overwrite JSON file if -j
     # ...
@@ -70,25 +71,40 @@ def init_move(args):
     raise NotImplementedError
     # TODO
 
+option_n = {
+    'action': 'store_true',
+    'default': False,
+    'help': "don't actually attempt server execution"
+}
+
+option_i = {
+    'action': 'store',
+    'dest': 'interactive',
+    'type': num_entries,
+    'metavar': "user|group|sudoer[:N>0]",
+    'default': None,
+    'help': "create/edit/validate one or more entries interactively"
+}
+
+option_d = {
+    'action': 'store',
+    'dest': 'ldif_out_path',
+    'metavar': 'PATH',
+    'default': None,
+    'help': "write the resulting LDIFs to a file at PATH instead of stdout (if the file already exists any new entries will be appended to any existing entries)"
+}
+
+option_j = {
+    'action': 'store',
+    'dest': 'json_out_path',
+    'metavar': 'PATH',
+    'default': None,
+    'help': "write a JSON representation of the resulting LDIFs to a file at PATH (if the file already exists any new entries will be appended to any existing entries)"
+}
+
 # Global parser and arguments
 parser_ldap = argparse.ArgumentParser(
     prog = 'ldap', description = 'Manage LDAP users, groups and sudoers'
-)
-parser_ldap.add_argument(
-    '-n', action = 'store_true', default = False,
-    help = "don't actually attempt server execution"
-)
-parser_ldap.add_argument(
-    '-d', action = 'store', dest = 'ldif_out_path', metavar = 'PATH',
-    default = None,
-    help = """write the resulting LDIFs to a file at PATH instead of stdout
-            (if the file already exists any new entries will be appended to any existing entries)"""
-)
-parser_ldap.add_argument(
-    '-j', action = 'store', dest = 'json_out_path', metavar = 'PATH',
-    default = None,
-    help = """write a JSON representation of the resulting LDIFs to a file at PATH
-            (if the file already exists any new entries will be appended to any existing entries)"""
 )
 
 # Add parsers for subcommands
@@ -96,25 +112,37 @@ subparsers = parser_ldap.add_subparsers(dest = 'subparser_name', help = 'sub-com
 
 # Sub parser and arguments for adding entries
 parser_add = subparsers.add_parser('add-entries', help = 'add-entries help')
-parser_add.add_argument(
-    '-i', action = 'store', dest = 'interactive', type = num_entries,
-    metavar = 'ENTRY-TYPE:N(>0)', default = None,
-    help = "create/edit/validate one or more entries interactively"
-)
+parser_add.add_argument('-n', **option_n)
+parser_add.add_argument('-i', **option_i)
+parser_add.add_argument('-d', **option_d)
+parser_add.add_argument('-j', **option_j)
+# add arguments here...
 parser_add.set_defaults(func = init_add)
 
 # Sub parser and arguments for deleting entries
 parser_delete = subparsers.add_parser('delete-entries', help = 'delete-entries help')
+parser_delete.add_argument('-n', **option_n)
+parser_delete.add_argument('-i', **option_i)
+parser_delete.add_argument('-d', **option_d)
+parser_delete.add_argument('-j', **option_j)
 # add arguments here...
 parser_delete.set_defaults(func = init_delete)
 
 # Sub parser and arguments for modifying entry attributes
 parser_modify = subparsers.add_parser('modify-entries', help = 'modify-entries help')
+parser_modify.add_argument('-n', **option_n)
+parser_modify.add_argument('-i', **option_i)
+parser_modify.add_argument('-d', **option_d)
+parser_modify.add_argument('-j', **option_j)
 # add arguments here...
 parser_modify.set_defaults(func = init_modify)
 
 # Sub parser and arguments for renaming/moving/copying entries
 parser_move = subparsers.add_parser('move-entries', help = 'move-entries help')
+parser_move.add_argument('-n', **option_n)
+parser_move.add_argument('-i', **option_i)
+parser_move.add_argument('-d', **option_d)
+parser_move.add_argument('-j', **option_j)
 # add arguments here...
 parser_move.set_defaults(func = init_move)
 
